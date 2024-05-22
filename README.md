30a977aae94f3328ecaf1902062107deecca9068
BiggestChris <stylianouchristopher8@gmail.com>
2024-05-22 20:07:50 +0100

    Final clean-up changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Coordination Board V1
  
+Run new_app.py in Python to see the working MVP. app.py still exists to showcase the original pseudo-code for the application.
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 
 app = Flask(__name__)
 
+# TODO: Handle opening/closing database connection
 def init_db():
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
@@ -14,16 +15,45 @@ def init_db():
 
 @app.route('/', methods=['GET'])
 def home():
+    # TODO: Read information in local storage
+    # TODO: Convert local storage information into a variable
+    # TODO: Pass variable into page
     return render_template("index.html")
 
-@app.route('/data', methods=['GET'])
-def get_data():
-    conn = sqlite3.connect('data.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM my_table')
-    rows = cursor.fetchall()
-    conn.close()
-    return jsonify(rows)
+@app.route('/download', methods=['GET', 'POST'])
+def current_data():
+    if request.method == 'POST':
+        # TODO: Open connection to online database
+        # TODO: Download info for other hospitals
+        # TODO: Write info to local storage
+        return redirect("/download")
+    else:
+        # TODO: Read information in local storage
+        # TODO: Convert local storage information into a variable
+        # TODO: Pass variable into page
+        return render_template("download.html")
+    
+
+@app.route('/upload', methods=['GET', 'POST'])
+def current_data():
+    if request.method == 'POST':
+        if """Save button pressed""":
+            # TODO: Take form submission data and write to variables
+            # TODO: Write info to local storage
+            return redirect("/upload")
+        if """Upload button pressed""":
+            # TODO: Take form submission data and write to variables
+            # TODO: Write info to local storage
+            # TODO: Open connection to online database
+            # TODO: Upload info for own hospital/station to database
+            # TODO: Close connection to online database
+            return redirect("/upload")
+    else:
+        # TODO: Read information in local storage
+        # TODO: Convert local storage information into a variable
+        # TODO: Pass variable into page
+        return render_template("download.html")
+
 
 if __name__ == '__main__':
     init_db()
